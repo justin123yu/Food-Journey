9cb41878d1880057c4942afeba70abec0f764d7d
Justin <justin123yu@gmail.com>
2023-07-12 23:07:40 -0700

    added airtable api

--- app/Resturant/page.js
+++ app/Resturant/page.js
@@ -2,6 +2,17 @@ import React from "react"
 import "../styles/home.css"
 
 export default function Resturant() {
+  async function getAirtable(){
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${process.env.AIR_TABLE_API}`
+      }
+    };
+    
+    const response = await fetch(`${process.env.AIR_TABLE_URL}`, options);
+    return response.json();
+  }
   return (
     <main>
       <div>
