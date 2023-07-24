997c24abbb887ad1619fceb24b21db15e14ef1b5
Justin <justin123yu@gmail.com>
2023-07-24 02:40:59 -0700

    Added Detailed View

---
+++ app/Resturant/[id]/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function DetailedResturants(){
+
+
+    return(<div> DETAILED VIEW </div>)
+}
--- app/Resturant/_Cards.js
+++ app/Resturant/_Cards.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import Image from 'next/image';
+import Link from 'next/link'
 
 async function getData() {
   var records;
@@ -44,6 +45,7 @@ export default function Resturant() {
             <div className="card-body">
               <h5 className="card-title text-center">{items.fields.Name}</h5>
               <p className="card-text">{items.fields.Comments}</p>
+              <Link className="btn btn-primary" href={{pathname:`/Resturant/${items.fields.Name}`}}> Details</Link>
             </div>
           </div>
         </div>)
