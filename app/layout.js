8213681f358ee61c7dd2d33deef007ef9e4105e7
Justin <justin123yu@gmail.com>
2023-07-23 21:57:51 -0700

    Reverted Script

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import Navbar from './_component/navbar'
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -7,7 +8,7 @@ export default function RootLayout({ children }) {
       <body>
       <Navbar></Navbar>
         {children}
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossOrigin="anonymous"></script>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossOrigin="anonymous"></Script>
         </body>
     </html>
   )
