bb9218f9c2de1d36bc4aed2ecc7a7a7a8270a2dc
Justin <justin123yu@gmail.com>
2023-07-29 14:54:16 -0700

    Added Create Function

--- app/Create/page.js
+++ app/Create/page.js
@@ -7,7 +7,6 @@ export default function Create() {
   const [rating, setRating] = useState(0);
   const [comment, setComments] = useState("");
 
-
   async function submitResturant(url) {
     const data = {
       "fields": {
@@ -30,7 +29,7 @@ export default function Create() {
       },
       body: JSON.stringify(data)
     };
-
+    
     await fetch("https://api.airtable.com/v0/appWF1wQ4ozIpCeq3/Resturant", options).then(() => {
       console.log("Submited to Airtable");
     }).catch((err) => console.error(err));
@@ -49,12 +48,12 @@ export default function Create() {
       body: formdata
     }
 
-    // await fetch("https://api.imgur.com/3/image", requestOptions)
-    //   .then(response => response.json())
-    //   .then(result => setPhoto(result.data.link))
-    //   .catch(error => console.log('error', error));
+    await fetch("https://api.imgur.com/3/image", requestOptions)
+      .then(response => response.json())
+      .then(result => setPhoto(result.data.link))
+      .catch(error => console.log('error', error));
 
-    // await submitResturant(photo)
+    await submitResturant(photo)
   }
 
   return (
@@ -64,25 +63,25 @@ export default function Create() {
         <form onSubmit={uploadToImgur}>
           <div className="row mb-3">
             <label className="form-label">Name of Restaurant</label>
-            <input type="text" className="form-control" id="Name"></input>
+            <input type="text" value={name} onChange={e => setName(e.target.value)} className="form-control" id="Name"></input>
           </div>
           <div className="row mb-3">
             <label className="form-label">Location</label>
-            <input type="text" className="form-control" id="Location"></input>
+            <input type="text" value={location} onChange={e => setLocation(e.target.value)} className="form-control" id="Location"></input>
           </div>
           <div className="row mb-3">
             <label className="form-label">Rating</label>
-            <input type="number" className="form-control" id="Rating"></input>
+            <input type="number" value={rating} onChange={e => setRating(e.target.value)} className="form-control" id="Rating"></input>
           </div>
           <div className="row mb-3">
             <label className="form-label">Comments</label>
-            <input type="text" className="form-control" id="Comments"></input>
+            <input type="text" className="form-control" value={comment} onChange={e => setComments(e.target.value)} id="Comments"></input>
           </div>
           <div className="row mb-3">
-            <input type="file" onChange={e => setPhoto(e.target.files[0])}></input>
+            <input type="file" accept="image/*" onChange={e => setPhoto(e.target.files[0])}></input>
           </div>
+          <button className="btn btn-primary text-center" type="submit">Submit</button>
         </form>
-        <button className="btn btn-primary text-center" type="submit">Submit</button>
       </div>
     </main>
   )
