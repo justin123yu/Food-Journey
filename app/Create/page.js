ebaf6a3f6bf71471c8256322f782f1f2a3df4429
Justin <justin741yu@gmail.com>
2023-08-02 14:04:47 -0700

    Fixing error

--- app/Create/page.js
+++ app/Create/page.js
@@ -9,7 +9,6 @@ export default function Create() {
       <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
       <iframe class="airtable-embed airtable-dynamic-height" 
       src="https://airtable.com/embed/appWF1wQ4ozIpCeq3/shrftNo8gtCahNnhh?backgroundColor=green" 
-      frameBorder="0"
        onmousewheel="" 
        width="100%" height="1115"></iframe>
     </main>
