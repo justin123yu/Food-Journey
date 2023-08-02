ff637d32a56700f2801dcd86cf09c269b34f27ad
Justin <justin741yu@gmail.com>
2023-08-02 14:03:28 -0700

    Added Automatic Sizing for frame

--- app/Create/page.js
+++ app/Create/page.js
@@ -6,9 +6,12 @@ export default function Create() {
 
   return (
     <main>
-        <div className="ratio ratio-1x1">
-          <iframe src="https://airtable.com/embed/appWF1wQ4ozIpCeq3/shrftNo8gtCahNnhh"></iframe>
-      </div>
+      <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
+      <iframe class="airtable-embed airtable-dynamic-height" 
+      src="https://airtable.com/embed/appWF1wQ4ozIpCeq3/shrftNo8gtCahNnhh?backgroundColor=green" 
+      frameBorder="0"
+       onmousewheel="" 
+       width="100%" height="1115"></iframe>
     </main>
   )
 }
